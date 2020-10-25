b6820ce0783fc35ee758b9c5a38da3546f8a846a
dultcher <35472235+dultcher@users.noreply.github.com>
2020-10-25 23:20:59 +0000

    YES

---
+++ source/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("nav.navbar.navbar-expand-lg.navbar-white").hover(function(){
+      $(this).function(
+          $("a.nav-link").css("color", "#000942")
+      );
+    });
+  });
+
+
+
+  $(document).ready(function(){
+    $("nav.navbar.navbar-expand-lg.navbar-white").ready(function(){
+      $(this).function(
+          $("a.nav-link").css("color", "#ffffff")
+      );
+    });
+  });
